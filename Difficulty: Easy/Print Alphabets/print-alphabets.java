399db2a6ef6336e33e19897458a4e063fa75ea1f
Himanshu kumar <himanshukumar1657@gmail.com>
2026-09-09 06:47:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Alphabets/print-alphabets.java
@@ -0,0 +1,15 @@
+// function should print the alphabets
+// from c1 to c2 (space separated)
+// No need to print the new line
+// The new line will be printed by the
+// driver's code.
+class Solution {
+    public static void alphabets(char c1, char c2) {
+        int n=(int)c2;
+        int m=(int)(c1);
+        for(int i=m;i<=n;i++){
+     System.out.print((char)i+" ");
+            }
+            
+        }
+    }
